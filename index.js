bd056574142d5a92851527cbbb27aefa5f6c43bf
unknown <Monirul-Jim>
2023-10-11 15:32:53 +0600

    fix update product pr

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
-app.use(cors())
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(cors(corsOptions))
 app.use(express.json());
 
 
